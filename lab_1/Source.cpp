86719b4b25bc7f15c1d3b06dce456651c218ae34
Wild-tapo4ek <153184896+Wild-tapo4ek@users.noreply.github.com>
2025-02-17 15:33:53 +0300

    Add files via upload

---
+++ lab_1/Source.cpp
@@ -0,0 +1,7 @@
+
+//chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
+//sort(v);
+//chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+//chrono::duration<double, milli> milli_diff = end - start;
+//cout << endl;
+//cout << "Time: " << milli_diff.count() << " milli sec." << endl;
